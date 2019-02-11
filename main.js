a5f6cc1027240c0b284ff7fe081f91d5e2dbf0cb
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-02-10 16:51:30 -0800

    run time

--- main.js
+++ main.js
@@ -5,10 +5,26 @@ function addUpTo(n) {
     }
     return total 
 }
-
 function addUpToo(n) {
  return n * (n + 1) / 2;
 }
 
 console.log(addUpTo(5))
 console.log(addUpToo(6))
+
+var t1 = performance.now();
+addUpToo(1000000000);
+var t2 = performance.now();
+console.log(`Time Passed: ${(t2 -t1) / 1000}seconds`)
+
+
+//run time complexities
+
+// O(1)                   Fastest
+// O(log)                    |
+// O(n)                      |
+// O(n log n)                v
+// O(n^2)                 Slowest
+
+
+
