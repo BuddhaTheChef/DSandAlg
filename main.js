fa226d3002bb37205521065a05b899804e1d67a0
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-02-12 15:21:26 -0800

    sum to zero

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ function same(arr1, arr2) {
 console.log(same([1,2,3,2], [4,1,4,9]))
 
 
-//ANAGRAMS//
+/////////////////////ANAGRAMS////////////////////////
 
 // SOLUTION 1
 function validAnagram(word1, word2) {
@@ -149,3 +149,19 @@ function validAnagram(first, second) {
   
   // {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
   validAnagram('anagrams', 'nagaramm')
+
+
+  //////////////////Multiple Pointers///////////////////////
+
+  function sumZero(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i+1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
+
+
+sumZero([-4,-3,-2,-1,0,1,2,5])
