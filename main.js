fcc1839108c5a2fe2ad2e9551705d4d7464726bb
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-02-12 15:09:37 -0800

    anagram solution

--- main.js
+++ main.js
@@ -76,3 +76,76 @@ console.log(`Time Passed: ${(t2 -t1) / 1000}seconds`)
 }
 
 console.log(charCount('Hello Hi'))
+
+function same(arr1, arr2) {
+    if(arr1.length !== arr2.length) {
+        return false
+    }
+    for(let i = 0; i < arr1.length; i++) {
+        let correctIndex = arr2.indexOf(arr1[i] ** 2)
+        if(correctIndex === -1) {
+            return false;
+        }
+        arr2.splice(correctIndex,1) 
+    }
+    return true;
+}
+
+console.log(same([1,2,3,2], [4,1,4,9]))
+
+
+//ANAGRAMS//
+
+// SOLUTION 1
+function validAnagram(word1, word2) {
+    var counts = [];
+    var word1Length = word1.length;
+    var word2Length = word2.length;
+  
+    if (word1Length !== word2Length) { return false; }
+  
+    for (var i = 0; i < word1Length; i++) {
+      var index = word1.charCodeAt(i)-97;
+      counts[index] = (counts[index] || 0) + 1;
+    }
+  
+    for (var j = 0; j < word2Length; j++) {
+      var index2 = word2.charCodeAt(j)-97;
+      if (!counts[index2]) { return false; }
+      else { counts[index2]--; }
+    }
+  
+    return true;
+  }
+
+//SOLUTION 2
+
+function validAnagram(first, second) {
+    if (first.length !== second.length) {
+      return false;
+    }
+  
+    const lookup = {};
+  
+    for (let i = 0; i < first.length; i++) {
+      let letter = first[i];
+      // if letter exists, increment, otherwise set to 1
+      lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+    console.log(lookup)
+  
+    for (let i = 0; i < second.length; i++) {
+      let letter = second[i];
+      // can't find letter or letter is zero then it's not an anagram
+      if (!lookup[letter]) {
+        return false;
+      } else {
+        lookup[letter] -= 1;
+      }
+    }
+  
+    return true;
+  }
+  
+  // {a: 0, n: 0, g: 0, r: 0, m: 0,s:1}
+  validAnagram('anagrams', 'nagaramm')
