4030eda3db4533c408e1a2eea5dc912cb6e5b20f
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-02-12 15:55:37 -0800

    max sum in array refactored

--- main.js
+++ main.js
@@ -182,3 +182,46 @@ function countUniqueValues(arr) {
 }
 
 console.log(countUniqueValues([1,1,2,2,2,3,3,4,5,5,6,6,6,7]))
+
+
+/////////////////////////MAX SUM///////////////////////////////
+
+//SOLUTION 1
+function maxSubarraySum(arr, num) {
+    if ( num > arr.length){
+      return null;
+    }
+    var max = -Infinity;
+    for (let i = 0; i < arr.length - num + 1; i ++){
+      temp = 0;
+      for (let j = 0; j < num; j++){
+        temp += arr[i + j];
+      }
+      if (temp > max) {
+        max = temp;
+      }
+    }
+    return max;
+  }
+  
+  console.log(maxSubarraySum([2,6,9,2,1,8,5,6,3],3))
+
+
+//SOLUTION 2
+  function maxSubarraySum(arr, num){
+    let maxSum = 0;
+    let tempSum = 0;
+    if (arr.length < num) return null;
+    for (let i = 0; i < num; i++) {
+      maxSum += arr[i];
+    }
+    tempSum = maxSum;
+    for (let i = num; i < arr.length; i++) {
+      tempSum = tempSum - arr[i - num] + arr[i];
+      maxSum = Math.max(maxSum, tempSum);
+    }
+    return maxSum;
+  }
+  
+  console.log(maxSubarraySum([2,6,9,2,1,8,5,6,3],3))
+  
