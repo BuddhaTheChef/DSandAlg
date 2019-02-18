ce43dbc8a83671c5b70adff2361a38006e1a7310
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-02-18 09:59:30 -0800

    added solutons to recursive problems

--- main.js
+++ main.js
@@ -555,6 +555,10 @@ function reverse(str){
 	if(str.length <= 1) return str;
 	return reverse(str.slice(1)) + str[0];
 }
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
+
+
 
 
 //isPalindrome Solution
@@ -564,6 +568,16 @@ function isPalindrome(str){
     if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
     return false;
 }
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
+
+
+
+
+
 
 //someRecursive Solution
 function someRecursive(array, callback) {
@@ -571,17 +585,33 @@ function someRecursive(array, callback) {
     if (callback(array[0])) return true;
     return someRecursive(array.slice(1),callback);
 }
+// SAMPLE INPUT / OUTPUT
+// const isOdd = val => val % 2 !== 0;
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
+
+
+
 
 
 //flatten Solution
-function flatten(oldArr){
-  var newArr = []
-  	for(var i = 0; i < oldArr.length; i++){
-    	if(Array.isArray(oldArr[i])){
-      		newArr = newArr.concat(flatten(oldArr[i]))
+function flatten(arr){
+  let newArr = [];
+  	for(let i in arr){
+    	if(Array.isArray(arr[i])){
+      		newArr = newArr.concat(flatten(arr[i]))
     	} else {
-      		newArr.push(oldArr[i])
+      		newArr.push(arr[i])
     	}
   } 
   return newArr;
 }
+
+
+// flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
+// flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+// flatten([[1],[2],[3]]) // [1,2,3]
+// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3
