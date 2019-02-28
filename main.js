fa9fa525a05f356b922f1648a69c11963507de66
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-02-28 15:36:50 -0800

    added in merge sort

--- main.js
+++ main.js
@@ -1002,7 +1002,6 @@ return arr;
 selectionSort([0,2,34,22,10,19,17]);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 //Insertion sort 
 
 function insertionSort(arr){
@@ -1018,3 +1017,35 @@ function insertionSort(arr){
 }
 
 insertionSort([2,1,9,76,4])
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Intermediate sorting algorithms
+function merge(arr1, arr2) {
+  let newArr = [];
+  let i = 0;
+  let j = 0;
+  while(i < arr1.length && j < arr2.length) {
+    if(arr2[j] > arr1[i]) {
+      newArr.push(arr1[i]);
+      i++;
+    }
+    else {
+      newArr.push(arr2[j]);
+      j++
+    }
+  }
+
+  while(i < arr1.length) {
+    newArr.push(arr1[i]);
+    i++;
+  }
+
+  while(j < arr2.length) {
+    newArr.push(arr2[j]);
+    j++;
+  }
+
+  return newArr;
+}
+
+console.log(merge([1,10,20,50], [2,14,19,100,103]))
