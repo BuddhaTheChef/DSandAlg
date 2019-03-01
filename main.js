59039d5c5009dea758848b54fa9ea3fb1bb53c30
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-03-01 14:30:47 -0800

    added in helper function for quick sort

--- main.js
+++ main.js
@@ -1020,6 +1020,7 @@ insertionSort([2,1,9,76,4])
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Intermediate sorting algorithms
+//MERGE SORT O(n log n)
 function merge(arr1, arr2) {
   let newArr = [];
   let i = 0;
@@ -1054,3 +1055,51 @@ function mergeSort(arr) {
 }
 
 console.log(mergeSort([1,10,20,50,2,14,19,100,103]))
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Quick Sort 
+
+// First Version
+function pivot(arr, start=0, end=arr.length+1){
+  function swap(array, i, j) {
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  var pivot = arr[start];
+  var swapIdx = start;
+
+  for(var i = start + 1; i < arr.length; i++){
+    if(pivot > arr[i]){
+      swapIdx++;
+      swap(arr,swapIdx,i);
+    }
+  }
+  swap(arr,start,swapIdx);
+  return swapIdx;
+}
+
+// Version with ES2015 Syntax
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  // We are assuming the pivot is always the first element
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i);
+    }
+  }
+
+  // Swap the pivot from the start the swapPoint
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+pivot([4,8,2,1,5,7,6,3])
