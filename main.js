9b9e73465786620c14b46bd1bfe0b3da85d3a2da
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-03-03 11:29:04 -0800

    added in radix sort

--- main.js
+++ main.js
@@ -1138,5 +1138,19 @@ function mostDigits(nums) {
   }
   return maxDigits;
 }
+//actual sort method O(nk)
+function radixSort(nums){
+  let maxDigitCount = mostDigits(nums);
+  for(let k = 0; k < maxDigitCount; k++){
+      let digitBuckets = Array.from({length: 10}, () => []);
+      for(let i = 0; i < nums.length; i++){
+          let digit = getDigit(nums[i],k);
+          digitBuckets[digit].push(nums[i]);
+      }
+      nums = [].concat(...digitBuckets);
+  }
+  return nums;
+}
 
-mostDigits([23,567,89,12234324,90])
+radixSort([23,345,5467,12,2345,9852])
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
